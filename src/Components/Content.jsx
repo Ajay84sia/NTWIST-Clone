712560b87e985b0ad1ac6d689da3397fab1b57d8
Ajay84sia <ajay9679339@gmail.com>
2023-11-06 14:57:16 +0530

    ntwist-app

---
+++ src/Components/Content.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Mine from "./Mine";
+import Mineral from "./Mineral";
+import Sustainablity from "./Sustainablity";
+import Oilgas from "./Oilgas";
+
+const Content = () => {
+  return (
+    <>
+      <Mine />
+      <Sustainablity />
+      <Mineral />
+      <Oilgas />
+    </>
+  );
+};
+
+export default Content;
